4f1a352bfd10244895e4bcf51207848b6a407804
lemuelpires <lemuelpires@gmail.com>
2024-09-14 07:54:21 -0300

    atualizacao migrations e do .net

---
+++ sigmaBack.DTOs/UsuarioDTOs.cs
@@ -0,0 +1,21 @@
+﻿;
+
+namespace sigmaBack.DTOs.DTOs
+{
+    public class UsuarioDto
+    {
+        public string? Email { get; set; }
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? Senha { get; set; }
+        public string? Genero { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Telefone { get; set; }
+        public string? CPF { get; set; }
+        public DateTime Data { get; set; }
+        public bool Ativo { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
+
+
